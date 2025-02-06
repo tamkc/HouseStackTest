1d4a5775f22902a7f29964bb878f1f32a22a46c1
tamkc <77198654+tamkc@users.noreply.github.com>
2025-02-06 17:45:21 -0500

    Integrate Notes module by adding controller and service, and configure Mongoose for Note schema

--- notes-api/src/app.module.ts
+++ notes-api/src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { NotesController } from './notes/notes.controller';
+import { NotesService } from './notes/notes.service';
+import { Note, NoteSchema } from './notes/schemas/note.schema';
 
 @Module({
   imports: [
@@ -14,6 +17,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }),
       inject: [ConfigService],
     }),
+    MongooseModule.forFeature([{ name: Note.name, schema: NoteSchema }]),
   ],
+  controllers: [NotesController],
+  providers: [NotesService],
 })
 export class AppModule {}
