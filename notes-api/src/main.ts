a2032650e82fc039d3adf336f0bfdb661760dc2e
tamkc <77198654+tamkc@users.noreply.github.com>
2025-02-06 21:40:54 -0500

    Configure HTTPS for the NestJS application and enhance CORS settings

--- notes-api/src/main.ts
+++ notes-api/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const httpsOptions = {
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert'),
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+
+  app.enableCors({
+    origin: 'https://main.d37oycb36yr5g.amplifyapp.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   await app.listen(3001);
 }
 void bootstrap();
